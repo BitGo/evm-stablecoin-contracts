6ce90ca4364369219de5f887d619bef0606ad9f2
Sachu Shaji Abraham <sachuabraham@bitgo.com>
2024-09-16 09:28:37 +0530

    chore: add natspec comments
    this change adds natspec comments
    to the contract
    
    Ticket: WIN-3453

--- contracts/Blacklistable.sol
+++ contracts/Blacklistable.sol
@@ -6,6 +6,10 @@ pragma solidity ^0.8.20;
 import "./openzeppelin/InternalERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol";
 
+/**
+ * @title Blacklistable
+ * @dev Contract that allows blacklisting of accounts.
+ */
 contract Blacklistable is
     AccessControlDefaultAdminRulesUpgradeable,
     InternalERC20Upgradeable
@@ -13,12 +17,21 @@ contract Blacklistable is
     event Blacklisted(address indexed account);
     event Unblacklisted(address indexed account);
 
+    /**
+     * @dev Checks if an account is blacklisted.
+     * @param account The address to check.
+     * @return A boolean indicating whether the account is blacklisted or not.
+     */
     function isBlacklisted(address account) public view returns (bool) {
         ERC20Storage storage $ = _getERC20Storage();
         return ($._balances[account] >> 255) == 1;
     }
 
-    // sets the last bit of _balances to 1 if state is true, 0 otherwise
+    /**
+     * @dev Sets the blacklist state of an account.
+     * @param account The address to set the blacklist state for.
+     * @param state The blacklist state to set.
+     */
     function _setBlacklistState(address account, bool state) internal {
         ERC20Storage storage $ = _getERC20Storage();
         if (state) {
@@ -30,31 +43,39 @@ contract Blacklistable is
         }
     }
 
-    function blacklist(
-        address account
-    ) public onlyRole(keccak256("BLACKLISTER_ROLE")) {
+    /**
+     * @dev Blacklists an account.
+     * @param account The address to blacklist.
+     */
+    function blacklist(address account) public onlyRole(keccak256("BLACKLISTER_ROLE")) {
         _setBlacklistState(account, true);
     }
 
-    function unblacklist(
-        address account
-    ) public onlyRole(keccak256("BLACKLISTER_ROLE")) {
+    /**
+     * @dev Unblacklists an account.
+     * @param account The address to unblacklist.
+     */
+    function unblacklist(address account) public onlyRole(keccak256("BLACKLISTER_ROLE")) {
         _setBlacklistState(account, false);
     }
 
-    // fetches balance of an account from the last 255 bits of _balances
-    function balanceOf(
-        address account
-    ) public view virtual override returns (uint256) {
+    /**
+     * @dev Fetches the balance of an account.
+     * @param account The address to fetch the balance for.
+     * @return The balance of the account.
+     */
+    function balanceOf(address account) public view virtual override returns (uint256) {
         ERC20Storage storage $ = _getERC20Storage();
         return $._balances[account] & ((1 << 255) - 1);
     }
 
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal virtual override {
+    /**
+     * @dev Updates the balances and total supply when a transfer occurs.
+     * @param from The address to transfer from.
+     * @param to The address to transfer to.
+     * @param value The amount to transfer.
+     */
+    function _update(address from, address to, uint256 value) internal virtual override {
         // Check if the sender is blacklisted, unless it's a minting operation (from == address(0))
         require(
             from == address(0) || !isBlacklisted(from),
